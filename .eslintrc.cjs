ece3f154614846c8f0a3c757b25ed57b0e2775a5
Kaipo <kaipojames12@gmail.com>
2021-10-16 15:47:45 -0500

    configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "globals": {
+        "__dirname": true
+    }
+};
