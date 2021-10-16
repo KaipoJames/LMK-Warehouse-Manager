fae7f5288108306c63eb5297ee8f89ed4d53c4c1
Kaipo <kaipojames12@gmail.com>
2021-10-16 15:49:37 -0500

    Add routes file

---
+++ app/config/routes/routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { productsProcessor } from "../../productsProcessor.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const params = { pageName: "home" };
+  const products = productsProcessor.getParsedProducts();
+  res.send(products);
+  //res.render("index", params);
+});
+router.get("/mongo-manager", (req, res) => {
+  const params = { pageName: "mongo", header: "DESCL Manager" };
+  //res.render("index", params);
+});
+router.get("/jobs-manager", (req, res) => {
+  const params = { pageName: "jobs", header: "DESCL Manager" };
+  //res.render("index", params);
+});
+
+export default router;
